d20189dbbbae26461d47c71a02ddc2e78016f745
EC2 Default User <ec2-user@ip-10-0-12-109.us-west-2.compute.internal>
2022-06-24 02:36:54 +0000

    Added

--- ToCelsius.py
+++ ToCelsius.py
@@ -1,3 +1,7 @@
-"""
-Your module description
-"""
+#!/usr/bin/env python.3.7
+
+#Python implement goes here
+
+fahrenheit = float(input("Input fahrenheit that you would like to convert: "))
+celsius = (fahrenheit -32) * 5 / 9
+print("The result is ", fahrenheit, "F", round(celsius, 2), "C")
