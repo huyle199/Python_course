ff70b37b25dbe3bdffa73571be76e264178893a2
EC2 Default User <ec2-user@ip-10-0-12-109.us-west-2.compute.internal>
2022-06-25 02:10:32 +0000

    Second run

--- First_Project.py
+++ First_Project.py
@@ -1,3 +1,14 @@
+#Create a list
 first_list = []
-first_list.append = "S3, Lambda, EC2"
-print first_list
+#Input the list by using append
+first_list.append("S3") 
+first_list.append("Lambda")
+first_list.append("EC2")
+#print List and separate it by a coma
+print(*first_list, sep= ",")
+
+
+#count the length and print the count
+length_list = len(first_list)
+print("The length of the list is: ",length_list)
+
