c4097d95da96d12fe6aaaada44a047a1e78649a0
EC2 Default User <ec2-user@ip-10-0-12-109.us-west-2.compute.internal>
2022-07-06 00:46:09 +0000

    a

--- tutorial.py
+++ tutorial.py
@@ -1,8 +1,8 @@
 import boto3
 aws_resource=boto3.resource("s3")
-bucket=aws_resource.Bucket("bigstorageforme")
+bucket=aws_resource.Bucket("bigstorageforme12")
 response = bucket.create(
-    ACL='public-read',
+    ACL='private',
     CreateBucketConfiguration={
         'LocationConstraint':'us-west-2'
     },
