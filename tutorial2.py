7d9e65ce4a563d87edd0180a6c01676bd941c5ab
EC2 Default User <ec2-user@ip-10-0-12-109.us-west-2.compute.internal>
2022-07-06 03:13:35 +0000

    a

--- tutorial2.py
+++ tutorial2.py
@@ -1,3 +1,8 @@
 import boto3
 resource=boto3.resource("s3")
-list(resource.buckets.all())
+
+bucket_list = list(resource.buckets.all())
+print (len(bucket_list))
+
+for bucket in resource.buckets.all():
+    print(bucket.name)
