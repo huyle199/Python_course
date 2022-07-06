e017d5972ee55dd676c9005dbc025f3413a35a38
huyle199 <touisuzuki@gmail.com>
2022-07-06 20:33:00 +0000

    Uploaded fixed code

--- tutorial7.py
+++ tutorial7.py
@@ -1,8 +1,8 @@
 import boto3
 s3_resource=boto3.client("s3")
-s3_resource=upload_file(
+s3_resource.upload_file(
     Filename="Huy Le Resume (8).pdf",
     #bucket name
-    Bucket="bigstorageforme"),
+    Bucket="bigstorageforme",
     #filename in the bucket
-    Key="Huy Le Resume (10).pdf")
+    Key="myimage.png")
