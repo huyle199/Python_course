c9049a500d063f782c3c7350001297bdc15bc2e5
huyle199 <touisuzuki@gmail.com>
2022-07-06 20:28:09 +0000

    a

---
+++ tutorial7.py
@@ -0,0 +1,8 @@
+import boto3
+s3_resource=boto3.client("s3")
+s3_resource=upload_file(
+    Filename="Huy Le Resume (8).pdf",
+    #bucket name
+    Bucket="bigstorageforme"),
+    #filename in the bucket
+    Key="Huy Le Resume (10).pdf"
